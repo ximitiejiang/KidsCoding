bb99a8b636e24c5c0d9bfaaed50c91df3a256318
ximitiejiang <ximitiejiang@163.com>
2018-11-10 23:03:28 +0800

    update lib

--- kc/draw.py
+++ kc/draw.py
@@ -5,16 +5,12 @@ Created on Sat Nov 10 11:36:23 2018
 
 @author: ubuntu
 
-? 是否支持self.a = 1的内部全局变量定义操作
-? 调用类里边其他函数，是否需要self.func()的形式，还是func()就可以了
-
 """
 
 import matplotlib.pyplot as plt
 
 class DRAW():
-    """绘图类：可用于绘制直线，圆，矩形，三角形
-    
+    """绘图类：可用于绘制直线，圆，矩形，三角形    
     1. color定义：
         'r' red红色
         'g' green绿色
@@ -23,9 +19,6 @@ class DRAW():
         'm' magenta品红
         'c' cyan蓝绿色
         'k' black黑色
-    2. line定义：
-    3. fill定义：
-    4.
     
     """
     
@@ -65,6 +58,7 @@ class DRAW():
         self.line(x3,x4,color=color)
         self.line(x4,x1,color=color)
     
+    
     def rectangle(self,x=[0.,0.], h=1.0, w=1.0, color='k',color_in=None):
         '''x为左下角坐标，h为高，w为宽
         '''
@@ -78,27 +72,47 @@ class DRAW():
         self.line(xrt,xrb,color=color)
         self.line(xrb,xlb,color=color)
         
-        
+        if color_in:
+            plt.fill_between([x[0],x[0]+w],
+                             [x[1],x[1]],
+                             [x[1]+h,x[1]+h], facecolor=color)        
         
     
-    def triangle(self, x1,x2,x3,color='m'):
+    def cal_triangle_midy(self,x):
+        '''input = ndarray (3x1) with x cordinate sorted.
+           output:
+               输出三角形中间点垂直切到对应边的切割点y坐标，该切割点x坐标与中间点相同
+        '''
+        x1 = x[0,0]
+        x2 = x[1,0]
+        x3 = x[2,0]
+        y1 = x[0,1]
+        y3 = x[2,1]
+        mid_y = (y3*(x2-x1) + y1*(x3-x2))/(x3-x1)
+        return mid_y
+        
+    
+    def triangle(self, x1,x2,x3,color='m',color_in=None):
         self.line(x1,x2,color=color)
         self.line(x2,x3,color=color)
         self.line(x3,x1,color=color)
         
-
+        if color_in:
+            import numpy as np
+            x = np.vstack((x1,x2,x3))
+            index = np.argsort(x[:,0])                 # x坐标从小到大排序
+            x_new = x[(index[0],index[1],index[2]),:]  # 调换坐标顺序
+            mid_y = self.cal_triangle_midy(x_new)
+            plt.fill_between(x_new[:,0],
+                             [x_new[0,1],mid_y,x_new[2,1]],
+                             x_new[:,1], 
+                             facecolor=color)        
     
 if __name__ == '__main__':
     draw = DRAW()
-#    draw.rectangle([1,1], 2, 2)
+#    plt.xlim(-1,8)
+#    plt.ylim(0,6)
+
+    draw.rectangle([1,1],w=3,h=4,color='g',color_in=True)
     
-#    plt.figure()
-#    draw.line([-2,-2], [3,3])
-    
-    draw.line([-2,-2],[4,4],color='r')
-    draw.ellipse([2,2],w=6,h=1.5,color='y',color_in=False,angle=45)
-   
-#    draw.circle([1,1],1,color='r',color_in=True)
-#    plt.figure()
-#    draw.rectangle([1,1],[2,1],[2,2],[1,2])
-#    draw.triangle([1,1],[3,1],[2,2])
+    draw.triangle([1,1],[4,2],[2,5],color='b',color_in=True)
